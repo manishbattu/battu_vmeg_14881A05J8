99b547342c8778367cecafdf07c5574f0c346b9f
manishbattu <31736555+manishbattu@users.noreply.github.com>
2017-09-08 15:54:45 +0530

    students

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		Student[] student=new Student[5];
+		StudentGroup sg=new StudentGroup(5);
+		sg.setStudents(student);
+		sg.getStudents();
+		
+	}
+
+}
